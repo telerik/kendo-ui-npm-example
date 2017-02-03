91b0c9792218e275eb6b6b6872e2f1dc444077cf
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2017-02-03 11:35:13 +0200

    feat: add JS + WebPack demo for Kendo UI Pro

---
+++ javascript-webpack/pro/main.js
@@ -0,0 +1,8 @@
+$ = require("jquery");
+require("@progress/kendo-ui/js/kendo.dropdownlist.js");
+
+require("@progress/kendo-ui/css/web/kendo.common.core.min.css");
+require("@progress/kendo-ui/css/web/kendo.default.min.css");
+
+$("#sel").kendoDropDownList();
+
