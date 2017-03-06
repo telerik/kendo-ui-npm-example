559a3edfd89e4cbcea03785a45e8e9605c1aab68
Stefan M. Stefanov <ststefan@progress.com>
2017-03-06 17:44:55 +0200

    chore: remove example with javascript and kendo ui core

--- javascript-webpack/core/main.js
+++
@@ -1,8 +0,0 @@
-$ = require("jquery");
-require("@progress/kendo-ui/js/kendo.dropdownlist.js");
-
-require("@progress/kendo-ui/css/web/kendo.common.core.min.css");
-require("@progress/kendo-ui/css/web/kendo.default.min.css");
-
-$("#sel").kendoDropDownList();
-
