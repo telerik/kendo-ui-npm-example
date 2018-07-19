92a42be41ae17e0a44f7720c3016d9f5ca819e98
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2018-07-19 10:59:47 +0300

    chore: add javascript-webpack core example

---
+++ javascript-webpack/core/main.js
@@ -0,0 +1,8 @@
+$ = require("jquery");
+require("kendo-ui-core/js/kendo.dropdownlist.js");
+
+require("kendo-ui-core/css/web/kendo.common.core.min.css");
+require("kendo-ui-core/css/web/kendo.default.min.css");
+
+$("#sel").kendoDropDownList();
+
